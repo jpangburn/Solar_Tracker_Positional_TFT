decce143e0aead7c35677d8c3ad51e13b1622875
jpangburn <jpangburn@neuraldirect.com>
2022-07-07 20:49:53 -0700

    install settings for physical tracker

--- install_specific_settings.h
+++ install_specific_settings.h
@@ -1,14 +1,14 @@
 // these settings are specific to a given solar tracker installation
 
 // what is the full west position
-const int FULL_WEST_POSITION = 2300;
+const int FULL_WEST_POSITION = 855;
 // what is the minimum amount of ticks to move?  (don't move if going to move less than this)
 const unsigned int MINIMUM_ACTUATOR_MOVEMENT = 10;
 // what is the lat/long of this tracker
 const float LATITUDE = 33.11;
 const float LONGITUDE = -116.98;
 // polynomial regression constants for converting desired azimuth to actuator position
-float X3 = -0.0010593807;
-float X2 = 0.6189061527;
-float X1 = -96.9547832692;
-float X0 = 4516.4461316026;
+float X3 = 0.0003685;
+float X2 = -0.2881681;
+float X1 = 81.6435101;
+float X0 = -7322;
